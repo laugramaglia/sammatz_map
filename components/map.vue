04dfd1ad61b765bceefa1b7650d6a73b1d212cc7
laugramaglia <laugramaglia@gmail.com>
2021-10-04 17:30:17 +0200

    heroku

--- components/map.vue
+++ components/map.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="map-wrap" style="height: 100vh">
     <client-only>
-      <l-map :zoom="3" :minZoom="3" :maxZoom="4" maxBoundsViscosity="1.0">
+      <l-map :zoom="3" :minZoom="2" :maxZoom="4" maxBoundsViscosity="1.0">
         <l-tile-layer url="/map/{z}/{x}/{y}.png" noWrap="true"></l-tile-layer>
         <PopUpEdited
           title="Title"
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Map/>
-    <FilterBar />
+    <FilterBar/>
   </div>
 </template>
 
