d642396465640e9a993f8ce6cb00cb88210373e8
Laureano Gramaglia <laugramaglia@gmail.com>
2021-10-04 11:30:16 +0200

    0.0.0.1

---
+++ components/map.vue
@@ -0,0 +1,14 @@
+<template>
+<div id="map-wrap" style="height: 100vh">
+ <client-only>
+   <l-map :zoom=3 :minZoom="3" :maxZoom="5" :center="[55.9464418,8.1277591]">
+     <l-tile-layer url="/map/{z}/{x}/{y}.png"></l-tile-layer>
+   </l-map>
+ </client-only>
+</div>
+</template>
+<script>
+export default{
+
+}
+</script>
