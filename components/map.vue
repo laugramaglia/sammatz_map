577f08d78f32ffdeccd5a776f60410c0df8ff139
laugramaglia <laugramaglia@gmail.com>
2021-10-04 14:46:18 +0200

    pop up edited

--- components/map.vue
+++ components/map.vue
@@ -1,14 +1,24 @@
 <template>
-<div id="map-wrap" style="height: 100vh">
- <client-only>
-   <l-map :zoom=3 :minZoom="3" :maxZoom="5" :center="[55.9464418,8.1277591]">
-     <l-tile-layer url="/map/{z}/{x}/{y}.png"></l-tile-layer>
-   </l-map>
- </client-only>
-</div>
+  <div id="map-wrap" style="height: 100vh">
+    <client-only>
+      <l-map :zoom="3" :minZoom="3" :maxZoom="5">
+        <l-tile-layer url="/map/{z}/{x}/{y}.png"></l-tile-layer>
+         <PopUpEdited title="Title" description="description lorem ipsum text long no sense text just to fill the
+              box." lat="0" lng="0" />
+        <l-control position="bottomleft">
+          <button @click="clickHandler">I am a useless button!</button>
+        </l-control>
+      </l-map>
+    </client-only>
+  </div>
 </template>
 <script>
-export default{
-
-}
+export default {
+  methods: {
+    clickHandler() {
+      window.alert("and mischievous");
+    },
+  },
+};
 </script>
+
---
+++ components/popUpEdited.vue
@@ -0,0 +1,48 @@
+<template>
+  <l-marker :lat-lng="[lat, lng]">
+    <l-popup class="pop-up-grid">
+      <a-row type="flex">
+        <a-col :span="8">
+          <div class="img-placeholder">
+            <img :src="img" alt="" />
+          </div>
+        </a-col>
+        <a-col :span="16">
+          <div class="text-content">
+            <h2>{{title}}</h2>
+            <p>
+              {{description}}
+            </p>
+          </div>
+        </a-col>
+      </a-row>
+    </l-popup>
+  </l-marker>
+</template>
+<script>
+export default {
+    props:[
+        "title",
+        "description",
+        "img",
+        "lat",
+        "lng"
+    ],
+};
+</script>
+<style lang="scss">
+.pop-up-grid {
+    width: 280px;
+  .img-placeholder {
+      background-color: red;
+      height: 100%;
+      border-radius: 8px;
+      margin-right: 12px;
+  }
+  .text-content{
+      p{
+          margin: 4px 0;
+      }
+  }
+}
+</style>
