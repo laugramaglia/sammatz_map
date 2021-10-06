a5b00659946b4ce630522fdf782c3dc5599faddb
laugramaglia <laugramaglia@gmail.com>
2021-10-06 17:01:40 +0200

    path 1

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,79 @@
 <template>
   <div id="map-wrap" style="height: 100vh">
     <client-only>
-      <l-map :center="[-50.466683, 43.683701]" :zoom="4" :minZoom="2" :maxZoom="7" :maxBoundsViscosity="1.0" @click="mapClick">
+      <l-map
+        :center="[-50.466683, 43.683701]"
+        :zoom="4"
+        :minZoom="2"
+        :maxZoom="7"
+        :maxBoundsViscosity="1.0"
+        @click="mapClick"
+      >
         <l-tile-layer url="/map/{z}/{x}/{y}.png" :noWrap="true"></l-tile-layer>
+        <!-- List of markers -->
         <div v-for="dataMark in dataGroups" :key="dataMark.router">
           <l-marker
-          v-for="mark in dataMark.markers"
-          :lat-lng="[mark.lat, mark.lng]"
-          :key="mark.name"
-          :visible="dataMark.visible"
-          
-        >
-        <l-icon
-          :icon-size="[22, 37]"
-          :icon-anchor="[16, 37]"
-          :icon-url='"/markers/"+dataMark.color+".svg"'
-         >
-</l-icon>
-          <PopUpEdited 
-          :title="mark.name" 
-          :description="mark.description"
-          :link="mark.link"
-          :img="mark.img"
-          />
-        </l-marker>
+            v-for="mark in dataMark.markers"
+            :lat-lng="[mark.lat, mark.lng]"
+            :key="mark.name"
+            :visible="dataMark.visible"
+          >
+            <l-icon
+              :icon-size="[22, 37]"
+              :icon-anchor="[16, 37]"
+              :icon-url="'/markers/' + dataMark.color + '.svg'"
+            >
+            </l-icon>
+            <!-- pop-up module -->
+            <PopUpEdited
+              :title="mark.name"
+              :description="mark.description"
+              :link="mark.link"
+              :img="mark.img"
+            />
+          </l-marker>
         </div>
+        <!-- Line path -->
+        <l-polyline
+          :lat-lngs="polylineBlue.latlngs"
+          :color="polylineBlue.color"
+        ></l-polyline>
       </l-map>
     </client-only>
+    <!-- bottom right menu -->
     <FilterBar @onTap="onClickBlue" :groupsFilter="dataGroups" />
   </div>
 </template>
 <script>
 import dataGroups from "../api/data"
+import bluePath from "static/data/path.js"
+
 export default {
   data() {
     return {
-      dataGroups
+      colectData: [], // helper
+      dataGroups,
+      polylineBlue: {
+        latlngs: bluePath,
+        color: "violet",
+      },
     };
   },
   methods: {
     onClickBlue(group) {
       this.dataGroups[group]["visible"] = !this.dataGroups[group]["visible"];
     },
-    mapClick(event){
+    mapClick(event) {
       /**
        * Get (lat, lng)
        */
-      let coordinates = event.latlng.toString()
-      console.log(coordinates)
-    }
-  }
 
+      let coordinates = [event.latlng.lat, event.latlng.lng];
+
+      this.colectData.push(coordinates);
+      console.log(this.colectData);
+    },
+  },
 };
 </script>
 <style lang="scss">
@@ -62,3 +85,5 @@ button.button {
   transform: translateX(-50%);
 }
 </style>
+
+
