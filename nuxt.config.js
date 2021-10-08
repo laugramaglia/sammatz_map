9939d2509f605ed40ce18aac76add21bf3e3b3c2
laugramaglia <laugramaglia@gmail.com>
2021-10-08 11:33:46 +0200

    server update

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,11 +26,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  // Server
-  server: {     
-    port: 8000, // default: 3000     
-    host: '0.0.0.0', // default: localhost   
-  }, 
+
 
   // Server middleware
   serverMiddleware: [
