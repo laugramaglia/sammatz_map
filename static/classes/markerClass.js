05ae233e3cea67503063b9fc7ca84d414a3c6eb9
laugramaglia <laugramaglia@gmail.com>
2021-10-11 14:13:16 +0200

    new map

--- static/classes/markerClass.js
+++ static/classes/markerClass.js
@@ -4,12 +4,14 @@ export default class MarkerClass {
     lat = null
     lng = null
     link = ""
+    img = ''
 
-    constructor(cName, cDescription, cLat, cLng, cLink){
+    constructor(cName, cDescription, cLat, cLng, cImg, cLink){
         this.name = cName
         this.description = cDescription
         this.lat = cLat
         this.lng = cLng
         this.link = cLink
+        this.img = cImg
     }
 }
